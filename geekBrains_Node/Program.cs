1a40721b4f4048da17834d0f8c723236ab4674b0
Александр Марфицын <nonesasha225@gmail.com>
2021-02-05 00:10:21 +0500

    Изменения1

--- geekBrains_Node/Program.cs
+++ geekBrains_Node/Program.cs
@@ -10,26 +10,59 @@ namespace geekBrains_Node
     {
         static void Main(string[] args)
         {
-            Node<int> node = Insert(new Node<int>(), 100);
-            //print(node);
+            Node<int> node = new Node<int>();
+            Insert(node, 10);
+            Insert(node, 12);
+            Insert(node, 8);
+            Insert(node, 9);
+            Insert(node, 7);
+            Insert(node, 3);
+            Insert(node, 4);
+            printTree(node, "root", 0);
+            Console.WriteLine("max = %d\n", getMaxNode(node).Data);
+            Console.WriteLine("min = %d\n", getMinNode(node).Data);
+            deleteValue(node, 4);
+            Console.WriteLine("parent of 7 is %d\n", getNodeByValue(node, 7).Parent.Data);
+            printTree(node, "root", 0);
+            deleteValue(node, 8);
+            printTree(node, "root", 0);
+            Console.WriteLine("------------------\n");
+            deleteValue(node, 10);
+            printTree(node, "root", 0);
+
             Console.ReadLine();
         }
 
-        static Node<int> GetFreeNode(int value, Node<int> head)
+        static void deleteValue(Node<int> node, int value)
         {
-            Node<int> node = new Node<int>();
-            node.Data = value;
-            node.Parent = head;
-            return node;
-
-
-
+            Node<int> target = getNodeByValue(node, value);
+            removeNodeByPtr(target);
         }
 
+        static void printTree(Node<int> node, string str, int level) {
+            if (node != null)
+            {
 
+                Console.WriteLine($"{level} {str} {node.Data}");
+                printTree(node.Left, "left", level+1);
+                printTree(node.Right, "right", level+1);
+            }
+    }
+
+        static Node<int> GetFreeNode(int value, Node<int> head)
+        {
+            Node<int> tmp = new Node<int>();
+            tmp.Left = null;
+            tmp.Right = null;
+            tmp.Data = value;
+            tmp.Parent = head;
+            return tmp;
+
+        }
         public static Node<int> Insert(Node<int> head, int value)
         {
             Node<int> tmp = null;
+
             if (head == null)
             {
                 head = GetFreeNode(value, null);
@@ -48,7 +81,7 @@ namespace geekBrains_Node
                     }
                     else
                     {
-                        tmp.Right = GetFreeNode(value, tmp); ;
+                        tmp.Right = GetFreeNode(value, tmp);
                         return head;
                     }
                 }
@@ -67,39 +100,94 @@ namespace geekBrains_Node
                 }
                 else
                 {
-                    throw new Exception("Wrong tree state");                  // Дерево построено неправильно
+                    throw new Exception("Wrong tree state");                  // ƒерево построено неправильно
                 }
             }
-            return tmp;
+            return head;
         }
-        private static Node<int> Search(Node<int> node, int value)
+        public static Node<int> getMinNode(Node<int> node)
         {
-            if (node == null) return null;
-            switch (value.CompareTo(node.Data))
+            while (node.Left != null)
             {
-                case 1: return Search(node.Right, value);
-                case -1: return Search(node.Left, value);
-                case 0: return node;
-                default: return null;
+                node = node.Left;
             }
+            return node;
         }
-        private static void _print(Node<int> node)
+
+        public static Node<int> getMaxNode(Node<int> node)
         {
-            if (node == null) return;
-            _print(node.Left);
-            node.listForPrint.Add(node.Data);
-            Console.Write(node + " ");
-            if (node.Right != null)
-                _print(node.Right);
+            while (node.Right != null)
+            {
+                node = node.Right;
+            }
+            return node;
         }
-        public static void print(Node<int> node)
+
+        public static Node<int> getNodeByValue(Node<int> node, int value)
         {
-            node.listForPrint.Clear();
-            _print(node);
-            Console.WriteLine();
+            while (node != null)
+            {
+                if (node.Data > value)
+                {
+                    node = node.Left;
+                    continue;
+                }
+                else if (node.Data < value) {
+                    node = node.Right;
+                    continue;
+                } else return node;
+            }
+            return null;
+        }
+
+        public static void removeNodeByPtr(Node<int> node)
+        {
+            if (node.Left != null && node.Right != null)
+            {
+                Node<int> localMax = getMaxNode(node.Left);
+                node.Data = localMax.Data;
+                removeNodeByPtr(localMax);
+                return;
+            }
+            else if (node.Left != null)
+            {
+                if (node == node.Parent.Left)
+                {
+                    node.Parent.Left = node.Left;
+                }
+                else
+                {
+                    node.Parent.Right = node.Left;
+                }
+            }
+            else if (node.Right != null)
+            {
+                if (node == node.Parent.Right)
+                {
+                    node.Parent.Right = node.Right;
+                }
+                else
+                {
+                    node.Parent.Left = node.Right;
+                }
+            }
+            else
+            {
+                if (node == node.Parent.Left)
+                {
+                    node.Parent.Left = null;
+                }
+                else
+                {
+                    node.Parent.Right = null;
+                }
+            }
+            return;
         }
 
     }
+
+}
     public class Node<T>
     {
         public T Data { get; set; }
@@ -107,6 +195,8 @@ namespace geekBrains_Node
         public Node<T> Right { get; set; }
         public Node<T> Parent { get; set; }
 
+        public bool visited { get; set; }
+
         public List<T> listForPrint = new List<T>();
     }
-}
+
